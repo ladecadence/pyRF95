80e290f85a8bc22cd56f3948cd3f16e79038df12
David Pello <dpello@ladecadence.net>
2016-11-13 16:17:38 +0000

    License and cleanup

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # pyRF95
+
+HopeRF RF95 (and similar LoRa modules) python library
+
+Based in RadioHead library code: http://www.airspayce.com/mikem/arduino/RadioHead/
+
--- rf95.py
+++ rf95.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python3
 
+# (C) 2016 David Pello Gonzalez for ASHAB
+# Based on code from the RadioHead Library:
+# http://www.airspayce.com/mikem/arduino/RadioHead/
+#
+# This program is free software: you can redistribute it
+# and/or modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation, either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  
+# If not, see <http://www.gnu.org/licenses/>.
+
 import time
 import sys
 import spidev
